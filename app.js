7f971ba70ae793590215bc06c078ec383a9c7e9d
Isaac Yi <iyi@isaacYi.fios-router.home>
2016-11-11 20:09:41 -0500

    added notes.txt

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 (function() {
-	//module named gemStore
+	//module named gemStore. Parameter = ('name', [array of dependencies])
 	var app = angular.module('gemStore', []);
 	//controller named StoreController
 	app.controller('StoreController', function(){
 	//code here is the ananoymous function and
 	//what gets executed when StoreController is called
-	this.product = gem;
+	this.products = gems;
 	});
-	var gem = {
-	name: 'Azurite',
-	price: 110.50,
-	canPurchase: true,
-	soldOut: true
-	};
+	var gems = [
+	{ name: 'Azurite', price: 110.50, canPurchase: true, soldOut: false },
+	{ name: 'Bloodstone', price: 5.95, canPurchase: false, soldOut: true },
+    { name: 'Zircon', price: 3.95, canPurchase: true, soldOut: false }
+	];
 })();
